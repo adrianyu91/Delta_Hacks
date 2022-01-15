867c961036b26c6101b6ca130eab17415ef73283
Aadi Sanghani <42876004+Aadi04@users.noreply.github.com>
2022-01-15 14:33:44 -0500

    Fixed Flask issue

---
+++ static/mainJs.js
@@ -0,0 +1,43 @@
+// var minutesLabel = document.getElementById("minutes");
+// var secondsLabel = document.getElementById("seconds");
+// var totalSeconds = 0;
+// setInterval(setTime, 1000);
+
+// function setTime()
+// {
+//     ++totalSeconds;
+//     secondsLabel.innerHTML = pad(totalSeconds%60);
+//     minutesLabel.innerHTML = pad(parseInt(totalSeconds/60));
+// }
+
+// function pad(val)
+// {
+//     var valString = val + "";
+//     if(valString.length < 2)
+//     {
+//         return "0" + valString;
+//     }
+//     else
+//     {
+//         return valString;
+//     }
+// }
+
+// setTime()
+
+// var timerVar = setInterval(countTimer, 1000);
+// var totalSeconds = 0;
+
+// function countTimer(){
+//     ++totalSeconds;
+
+//     var hour = Math.floor(totalSeconds/3600)
+//     var minute = Math.floor((totalSeconds - hour * 3600) / 60);
+//     var seconds = totalSeconds - (hour*360 + minute * 60);
+
+//     document.getElementById("MyClockDisplay").innerHTML = minute + ":" + seconds;
+// }
+
+// countTimer();
+
+alert("hello")
