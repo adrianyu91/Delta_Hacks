204124f5483d89e12eeeb73f8083210b9a97562e
Kalp <kalpshah2003@gmail.com>
2022-01-15 20:18:20 -0500

    fixed timer buttons

--- static/mainJs.js
+++ static/mainJs.js
@@ -1,44 +1,76 @@
 // var timerVar = setInterval(countTimer, 10000);
 var totalSeconds = 0;
+var check = false;
 
 function countTimer(){
-    ++totalSeconds;
-
-    var hour = Math.floor(totalSeconds/3600)
-    var minute = Math.floor((totalSeconds - hour * 3600) / 60);
-    var seconds = totalSeconds - (hour*360 + minute * 60);
-    minute_str = minute;
-    second_str = seconds;
-    if (minute <= 9) {
-        minute_str = "0" + minute_str;
+    if (check) {
+        ++totalSeconds;
+    
+        var hour = Math.floor(totalSeconds/3600)
+        var minute = Math.floor((totalSeconds - hour * 3600) / 60);
+        var seconds = totalSeconds - (hour*360 + minute * 60);
+        minute_str = minute;
+        second_str = seconds;
+        if (minute <= 9) {
+            minute_str = "0" + minute_str;
+        }
+        if (seconds <= 9) {
+            second_str = "0" + second_str;
+        }
+    
+        document.getElementById("MyClockDisplay").innerHTML = minute_str + ":" + second_str;
     }
-    if (seconds <= 9) {
-        second_str = "0" + second_str;
-    }
-
-    document.getElementById("MyClockDisplay").innerHTML = minute_str + ":" + second_str;
 }
 
-function start(x){
-
-    if (x==true){
+function start(x) {
+    
+    if (x == true) {
 
         document.getElementById("StartBtn").onclick = function() {
-            this.disabled = true;
+            document.getElementById("StartBtn").disabled = true;
+            document.getElementById("StopBtn").disabled = false;
+            check = true;
+            if (totalSeconds == 0) {
+                var timerVar = setInterval(this.countTimer, 1000);
+            }
+            totalSeconds = 0;
         }
         document.getElementById("StopBtn").onclick = function() {
-            this.disabled = false;
+            document.getElementById("StartBtn").disabled = false;
+            document.getElementById("StopBtn").disabled = true;
+            check = false;
         }
 
-    var timerVar = setInterval(countTimer, 1000);
+        document.getElementById("StartBtn").disabled = true;
+        document.getElementById("StopBtn").disabled = false;
+        check = true;
+        if (totalSeconds == 0) {
+            var timerVar = setInterval(this.countTimer, 1000);
+        }
+        totalSeconds = 0;
     }
-    else if(x==false){
-         document.getElementById("StartBtn").onclick = function() {
-             this.disabled = false;
-         }
+    else {
+        document.getElementById("StartBtn").onclick = function() {
+            document.getElementById("StartBtn").disabled = true;
+            document.getElementById("StopBtn").disabled = false;
+            check = true;
+            if (totalSeconds == 0) {
+                var timerVar = setInterval(this.countTimer, 1000);
+            }
+            totalSeconds = 0;
+        }
         document.getElementById("StopBtn").onclick = function() {
-          this.disabled = true;
-    }
+            document.getElementById("StartBtn").disabled = false;
+            document.getElementById("StopBtn").disabled = true;
+            check = false;
+        }
+        check = false
+        if (totalSeconds == 0) {
+            var timerVar = setInterval(this.countTimer, 1000);
+        }
+        totalSeconds = 0;
+        document.getElementById("StartBtn").disabled = false;
+        document.getElementById("StopBtn").disabled = true;
     }
 }
 
